08bd79b22aad1329a9c39c1e4d394b00ea8f6db7
ChoiWonOong <97601348+ChoiWonOong@users.noreply.github.com>
2025-07-14 16:47:40 +0900

    Title: 행복한 수열의 개수, Time: 235ms, Memory: 15MB, Status: Passed - Codetree

--- 250714/행복한 수열의 개수/number-of-happy-sequence.java
+++ 250714/행복한 수열의 개수/number-of-happy-sequence.java
@@ -12,27 +12,40 @@ public class Main {
                 grid[i][j] = sc.nextInt();
         
         // Please write your code here.
-        // n - m 번
         int cnt = 0;
-        for(int i=0; i<n-m; i++){
-            for(int j=0; j<n-m; j++){
+
+        // n번
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=n-m; j++){
                 int v = grid[i][j];
-                // 가로만
-                // m 번만 나오면 break 로 다음 열로
-                for(int k = 0; k<m; k++){
-                    if(grid[i][j+k] == v){
-                        // 같은 수가 m 번 반복
-                        cnt++;
-                        break;
-                    }
-                }
-                // 세로만
+                int tempCnt = 0;
                 for(int k=0; k<m; k++){
-                    if(grid[i+k][j] == v){
-                        cnt++;
-                        break;
+                    if(grid[i][j+k]==v){
+                        tempCnt++;
                     }
                 }
+                if(tempCnt==m){
+                    cnt++;
+                    break;
+                }
+                
+            }
+        }
+        // n번
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=n-m; j++){
+                int v = grid[j][i];
+                int tempCnt = 0;
+                for(int k=0; k<m; k++){
+                    if(grid[j+k][i]==v){
+                        tempCnt++;
+                    }
+                }
+                if(tempCnt==m){
+                    cnt++;
+                    break;
+                }
+                
             }
         }
         System.out.println(cnt);
