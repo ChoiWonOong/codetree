3bb16fb26009b8266ca88268084363d139938f46
ChoiWonOong <97601348+ChoiWonOong@users.noreply.github.com>
2025-07-30 17:01:22 +0900

    Title: 황금비율 토스트, Time: 3093ms, Memory: 187MB, Status: Time Limit Exceed - Codetree

--- 250730/황금비율 토스트/golden-toast.java
+++ 250730/황금비율 토스트/golden-toast.java
@@ -1,51 +1,51 @@
-import java.util.Scanner;
-import java.util.*;
+    import java.util.Scanner;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String s = sc.next();
-        sc.nextLine();
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            String s = sc.next();
+            sc.nextLine();
 
-        LinkedList<Character> list = new LinkedList<>();
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            list.add(c);
-        }
-        ListIterator<Character> iter = list.listIterator();
-        while(iter.hasNext()){
-            iter.next();
-        }
-        for (int i = 0; i < m; i++) {
-            String command = sc.nextLine();
-            String[] tokens = command.split(" ");
-            if(tokens[0].equals("R")){
-                if(iter.hasNext()){
-                    iter.next();
+            LinkedList<Character> list = new LinkedList<>();
+            for(int i=0; i<s.length(); i++){
+                char c = s.charAt(i);
+                list.add(c);
+            }
+            ListIterator<Character> iter = list.listIterator();
+            while(iter.hasNext()){
+                iter.next();
+            }
+            for (int i = 0; i < m; i++) {
+                String command = sc.nextLine();
+                String[] tokens = command.split(" ");
+                if(tokens[0].equals("R")){
+                    if(iter.hasNext()){
+                        iter.next();
+                    }
+                }else if(tokens[0].equals("L")){
+                    if(iter.hasPrevious()){
+                        iter.previous();
+                    }
+                }else if(tokens[0].equals("D")){
+                    if(iter.hasNext()){
+                        iter.next();
+                        iter.remove();
+                    }
+                }else if(tokens[0].equals("P")){
+                    iter.add((char)tokens[1].charAt(0));
+                }else{
+                    System.out.println("Wrong Command : " + command);
                 }
-            }else if(tokens[0].equals("L")){
-                if(iter.hasPrevious()){
-                    iter.previous();
-                }
-            }else if(tokens[0].equals("D")){
-                if(iter.hasNext()){
-                    iter.next();
-                    iter.remove();
-                }
-            }else if(tokens[0].equals("P")){
-                iter.add((char)tokens[1].charAt(0));
-            }else{
-                System.out.println("Wrong Command : " + command);
+            }
+            
+            // Please write your code here.
+            iter = list.listIterator();
+            while(iter.hasNext()){
+                System.out.print(iter.next());
             }
         }
-        
-        // Please write your code here.
-        iter = list.listIterator();
-        while(iter.hasNext()){
-            System.out.print((char)iter.next());
-        }
     }
-}
