b7e2b2155a5d9a75b77b2036e9763702a84464b4
ChoiWonOong <97601348+ChoiWonOong@users.noreply.github.com>
2026-06-30 22:47:13 +0900

    [Passed] 연속 부분 합의 최댓값 구하기 (839ms, 33MB)

---
+++ trail4/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            dp[i] = Math.max(dp[i-1] +nums[i], nums[i]);
+        }
+        int min = -1000;
+        for(int i=0; i<n; i++){
+            min = Math.max(dp[i], min);
+        }
+        System.out.println(min);
+    }
+}
